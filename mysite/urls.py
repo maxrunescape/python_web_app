e5dda300b63bc2a661bc1fb1e10ed107fe839a88
Jerry Feng <jerry.zj.feng@gmail.com>
2017-06-27 14:48:15 -0400

    basic authentications

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import snippets
+
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^polls_api/', include('polls_api.urls')),
     url(r'^snippets/', include('snippets.urls')),
+    url(r'^users/$', snippets.views.UserList.as_view()),
+	url(r'^users/(?P<pk>[0-9]+)/$', snippets.views.UserDetail.as_view()),
+	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
